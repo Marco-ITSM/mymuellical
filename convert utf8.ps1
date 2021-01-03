5b101c6fb11d836397d2887ec99a36f3ab27f24f
Marco Hahnen <marco.hahnen@itsm.de>
2021-01-03 21:28:27 +0100

    ics upload

--- convert utf8.ps1
+++
@@ -1,8 +0,0 @@
-$icsnew = "C:\Users\HahnenMarco\OneDrive - Marco Hahnen\ps\mymuellical\ics"
-$icsold = "C:\Users\HahnenMarco\OneDrive - Marco Hahnen\ps\mymuellical\ics-old"
-
-cd $icsold
-foreach( $i in get-childitem -recurse -name  ) {
-    get-content $i | out-file -encoding utf8 -filepath $icsnew/$i
-    $i
-}
